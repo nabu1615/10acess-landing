b14b59c7f2155b1c312336c07a3b07d1c3dba876
Javier <javier1615@gmail.com>
2020-05-01 19:02:31 -0500

    video

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
-var elem = document.querySelector(".testimonies");
-var flkty = new Flickity(elem, {
+const elem = document.querySelector(".testimonies");
+const flkty = new Flickity(elem, {
   wrapAround: true,
   prevNextButtons: false,
 });
@@ -17,4 +17,30 @@ $form.addEventListener("submit", (e) => {
       $form.reset();
     })
     .catch((error) => console.error("Error!", error.message));
+});
+
+const playButton = document.querySelector(".play");
+const videoDialog = document.querySelector(".props__video");
+const closeButton = document.querySelector(".close");
+const body = document.querySelector("body");
+
+// "Update details" button opens the <dialog> modally
+playButton.addEventListener("click", function onOpen() {
+  if (typeof videoDialog.showModal === "function") {
+    videoDialog.showModal();
+    window.scrollTo(500, 0);
+    body.classList.add("block");
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
+
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+videoDialog.addEventListener("close", () => {
+  body.classList.remove("block");
+});
+
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+closeButton.addEventListener("click", () => {
+  videoDialog.close();
 });
